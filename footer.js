63a2f0da4aef3a8fc2c06887653af0045442cce8
Matt Argo <mattdude1081@gmail.com>
2023-04-22 00:06:40 -0700

    Font size increase

--- footer.js
+++ footer.js
@@ -7,9 +7,8 @@ class footer extends HTMLElement {
                 <ul class="spacing-small-top no-bullet">
                     <li class="spacing-xsmall-bottom"><a href="mailto:mattargomaniz@gmail.com">Send me an email.</a></li>
                      <li class="spacing-xsmall-bottom"><a target="_blank" href="https://margomaniz.tumblr.com/">Lurk on my Tumblr.</a></li>
-                     <li class="spacing-xsmall-bottom"><a target="_blank" href="https://github.com/argomod">View my code on GitHub.</a></li>
+                     <li><a target="_blank" href="https://github.com/argomod">View my code on GitHub.</a></li>
                 </ul>
-                 <p class="spacing-medium-top small-text sec-text">Site updated April 2023.</p>
              </footer>
         `
     }
