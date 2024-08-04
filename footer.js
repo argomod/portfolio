05032af9f37f8b1a199767e08f23ffbe35ee3236
Matt Argo <mattdude1081@gmail.com>
2024-08-03 22:57:33 -0700

    Updated interests section iand colors

--- footer.js
+++ footer.js
@@ -3,14 +3,14 @@ class footer extends HTMLElement {
         super();
         this.innerHTML = `
         <footer>
-            <div class="currently-container spacing-medium-bottom">
-                <a class="currently-item currently-item-1" href="https://www.ourpaleolife.com/rustic-landscape-timber-trail-stairs/" target="_blank">
+            <div class="interest-container spacing-large-bottom">
+                <a class="interest-item interest-item-1" href="https://www.ourpaleolife.com/rustic-landscape-timber-trail-stairs/" target="_blank">
                     What I'm making <span aria-hidden="true">→</span>
                 </a>
-                <a class="currently-item currently-item-2" href="https://terremoto.la/" target="_blank">
+                <a class="interest-item interest-item-2" href="https://terremoto.la/" target="_blank">
                     What I'm studying <span aria-hidden="true">→</span>
                 </a>
-                <a class="currently-item currently-item-3" href="https://youtu.be/0rFVVzavii0?feature=shared" target="_blank">
+                <a class="interest-item interest-item-3" href="https://youtu.be/0rFVVzavii0?feature=shared" target="_blank">
                     What I'm listening to <span aria-hidden="true">→</span>
                 </a>
             </div>
