07bb306849ed0ebf16e12d04b9e551b4cb91ef7d
Matt Argo <mattdude1081@gmail.com>
2026-01-21 17:40:29 -0800

    Updated interests

--- footer.js
+++ footer.js
@@ -4,13 +4,13 @@ class footer extends HTMLElement {
         this.innerHTML = `
         <footer>
             <div class="interest-container spacing-large-bottom">
-                <a class="interest-item interest-item-1" href="https://www.ourpaleolife.com/rustic-landscape-timber-trail-stairs/" target="_blank">
+                <a class="interest-item interest-item-1" href="https://www.ikea.com/us/en/p/gulliver-crib-green-70589505/?gad_source=1&gad_campaignid=22399713111&gbraid=0AAAAAD27g7wzDtrmdIqdqxcpS6LpnGPsk&gclid=CjwKCAiAj8LLBhAkEiwAJjbY75esh6x-XrbTZexUqpaabN9MwAqAhCDjHK_BD1oOytkj7zO_C8P23hoC2zwQAvD_BwE" target="_blank">
                     What I'm making <span aria-hidden="true">→</span>
                 </a>
-                <a class="interest-item interest-item-2" href="https://terremoto.la/" target="_blank">
+                <a class="interest-item interest-item-2" href="https://www.hackingwithswift.com/quick-start/swiftui/how-to-add-metal-shaders-to-swiftui-views-using-layer-effects" target="_blank">
                     What I'm studying <span aria-hidden="true">→</span>
                 </a>
-                <a class="interest-item interest-item-3" href="https://youtu.be/0rFVVzavii0?feature=shared" target="_blank">
+                <a class="interest-item interest-item-3" href="https://youtu.be/AgCTkQdArwk?si=t4Jgk19xCaZW5P1b" target="_blank">
                     What I'm listening to <span aria-hidden="true">→</span>
                 </a>
             </div>
